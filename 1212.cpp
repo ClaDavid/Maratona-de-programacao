534f95bce745f6346ef6d4e33a459da147914603
ClaDavid <clarissa.david05@gmail.com>
2016-08-24 18:06:47 -0300

    Add files via upload

---
+++ 1212.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+
+main(){
+    
+    int s1, s2;
+    
+    
+    while((cin >> s1 && s1 != 0) && (cin >> s2 && s2 != 0))
+    
+    
+    return 0;
+}
